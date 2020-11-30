a0606d626c1a63826ee018156bd34dc76afea536
Evgeniy Uskov <driftrboy@gmail.com>
2020-11-30 08:14:39 +0700

    Initial Commit

---
+++ RxSwiftClimaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftClimaApp
+//
+//  Created by Evgeniy Uskov on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
